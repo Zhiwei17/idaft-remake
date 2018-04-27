027ac3857498872bbf9661b02a914221e9e02241
Alumnes <Alumnes@inf4.cesnuria>
2018-04-27 16:50:41 +0200

    usless if gone :astonished:
    audio ended listener not working :angry:

--- js/scripto.js
+++ js/scripto.js
@@ -1,240 +1,184 @@
 $(document).ready(function () {
 
+    //-------------------------------------------1
     //-------------------------------------------------------1.1
     $("#work_it_1").click(function () {
-        if (au_work_it_1.paused == false) {
-            au_work_it_1.pause();
-        } else {
-            au_work_it_1.cloneNode(true).play();
-        }
+
+        au_work_it_1.cloneNode(true).play();
+
     })
     $("#make_it_1").click(function () {
-        if (au_make_it_1.paused == false) {
-            au_make_it_1.pause();
-        } else {
-            au_make_it_1.cloneNode(true).play();
-        }
+
+        au_make_it_1.cloneNode(true).play();
+
     })
     $("#do_it_1").click(function () {
-        if (au_do_it_1.paused == false) {
-            au_do_it_1.pause();
-        } else {
-            au_do_it_1.cloneNode(true).play();
-        }
+
+        au_do_it_1.cloneNode(true).play();
+
     })
     $("#makes_us_1").click(function () {
-        if (au_makes_us_1.paused == false) {
-            au_makes_us_1.pause();
-        } else {
-            au_makes_us_1.cloneNode(true).play();
-        }
+
+        au_makes_us_1.cloneNode(true).play();
+
     })
 
     //-------------------------------------------------------1.2
     $("#harder_1").click(function () {
-        if (au_harder_1.paused == false) {
-            au_harder_1.pause();
-        } else {
-            au_harder_1.cloneNode(true).play();
-        }
+
+        au_harder_1.cloneNode(true).play();
+
     })
     $("#better_1").click(function () {
-        if (au_better_1.paused == false) {
-            au_better_1.pause();
-        } else {
-            au_better_1.cloneNode(true).play();
-        }
+
+        au_better_1.cloneNode(true).play();
+
     })
     $("#faster_1").click(function () {
-        if (au_faster_1.paused == false) {
-            au_faster_1.pause();
-        } else {
-            au_faster_1.cloneNode(true).play();
-        }
+
+        au_faster_1.cloneNode(true).play();
+
     })
     $("#stronger_1").click(function () {
-        if (au_stronger_1.paused == false) {
-            au_stronger_1.pause();
-        } else {
-            au_stronger_1.cloneNode(true).play();
-        }
+
+        au_stronger_1.cloneNode(true).play();
+
     })
     //-------------------------------------------------------1.3
     $("#more_than_1").click(function () {
-        if (au_more_than_1.paused == false) {
-            au_more_than_1.pause();
-        } else {
-            au_more_than_1.cloneNode(true).play();
-        }
+
+        au_more_than_1.cloneNode(true).play();
+
     })
     $("#hour_1").click(function () {
-        if (au_hour_1.paused == false) {
-            au_hour_1.pause();
-        } else {
-            au_hour_1.cloneNode(true).play();
-        }
+
+        au_hour_1.cloneNode(true).play();
+
     })
     $("#our_1").click(function () {
-        if (au_our_1.paused == false) {
-            au_our_1.pause();
-        } else {
-            au_our_1.cloneNode(true).play();
-        }
+
+        au_our_1.cloneNode(true).play();
+
     })
     $("#never_1").click(function () {
-        if (au_never_1.paused == false) {
-            au_never_1.pause();
-        } else {
-            au_never_1.cloneNode(true).play();
-        }
+
+        au_never_1.cloneNode(true).play();
+
     })
-    //-------------------------------------------------------4
+    //-------------------------------------------------------1.4
     $("#ever_1").click(function () {
-        if (au_ever_1.paused == false) {
-            au_ever_1.pause();
-        } else {
-            au_ever_1.cloneNode(true).play();
-        }
+
+        au_ever_1.cloneNode(true).play();
+
     })
     $("#after_1").click(function () {
-        if (au_after_1.paused == false) {
-            au_after_1.pause();
-        } else {
-            au_after_1.cloneNode(true).play();
-        }
+
+        au_after_1.cloneNode(true).play();
+
     })
     $("#work_is_1").click(function () {
-        if (au_work_is_1.paused == false) {
-            au_work_is_1.pause();
-        } else {
-            au_work_is_1.cloneNode(true).play();
-        }
+
+        au_work_is_1.cloneNode(true).play();
+
     })
     $("#over_1").click(function () {
-        if (au_over_1.paused == false) {
-            au_over_1.pause();
-        } else {
-            au_over_1.cloneNode(true).play();
-        }
+
+        au_over_1.cloneNode(true).play();
+
     })
 
-    //--------------------------------------2.1
-    //-------------------------------------------------------1
+    //--------------------------------------2
+    //-------------------------------------------------------2.1
     $("#work_it_2").click(function () {
-        if (au_work_it_2.paused == false) {
-            au_work_it_2.pause();
-        } else {
-            au_work_it_2.cloneNode(true).play();
-        }
+        au_work_it_2.cloneNode(true).play();
+            
+        
+        //-----------------------------------------------------------------FUCK. <------------ AQUÍ
+        au_work_it_2.addEventListener("ended", function () {
+            alert("this shit is not workiiiiiinnnngggg lalallalalal");
+        })
+
+
+
     })
     $("#make_it_2").click(function () {
-        if (au_make_it_2.paused == false) {
-            au_make_it_2.pause();
-        } else {
-            au_make_it_2.cloneNode(true).play();
-        }
+
+        au_make_it_2.cloneNode(true).play();
+
     })
     $("#do_it_2").click(function () {
-        if (au_do_it_2.paused == false) {
-            au_do_it_2.pause();
-        } else {
-            au_do_it_2.cloneNode(true).play();
-        }
+
+        au_do_it_2.cloneNode(true).play();
+
     })
     $("#makes_us_2").click(function () {
-        if (au_makes_us_2.paused == false) {
-            au_makes_us_2.pause();
-        } else {
-            au_makes_us_2.cloneNode(true).play();
-        }
+
+        au_makes_us_2.cloneNode(true).play();
+
     })
 
-    //-------------------------------------------------------2
+    //-------------------------------------------------------2.2
     $("#harder_2").click(function () {
-        if (au_harder_2.paused == false) {
-            au_harder_2.pause();
-        } else {
-            au_harder_2.cloneNode(true).play();
-        }
+
+        au_harder_2.cloneNode(true).play();
+
     })
     $("#better_2").click(function () {
-        if (au_better_2.paused == false) {
-            au_better_2.pause();
-        } else {
-            au_better_2.cloneNode(true).play();
-        }
+
+        au_better_2.cloneNode(true).play();
+
     })
     $("#faster_2").click(function () {
-        if (au_faster_2.paused == false) {
-            au_faster_2.pause();
-        } else {
-            au_faster_2.cloneNode(true).play();
-        }
+
+        au_faster_2.cloneNode(true).play();
+
     })
     $("#stronger_2").click(function () {
-        if (au_stronger_2.paused == false) {
-            au_stronger_2.pause();
-        } else {
-            au_stronger_2.cloneNode(true).play();
-        }
+
+        au_stronger_2.cloneNode(true).play();
+
     })
-    //-------------------------------------------------------3
+    //-------------------------------------------------------2.3
     $("#more_than_2").click(function () {
-        if (au_more_than_2.paused == false) {
-            au_more_than_2.pause();
-        } else {
-            au_more_than_2.cloneNode(true).play();
-        }
+
+        au_more_than_2.cloneNode(true).play();
+
     })
     $("#hour_2").click(function () {
-        if (au_hour_2.paused == false) {
-            au_hour_2.pause();
-        } else {
-            au_hour_2.cloneNode(true).play();
-        }
+
+        au_hour_2.cloneNode(true).play();
+
     })
     $("#our_2").click(function () {
-        if (au_our_2.paused == false) {
-            au_our_2.pause();
-        } else {
-            au_our_2.cloneNode(true).play();
-        }
+
+        au_our_2.cloneNode(true).play();
+
     })
     $("#never_2").click(function () {
-        if (au_never_2.paused == false) {
-            au_never_2.pause();
-        } else {
-            au_never_2.cloneNode(true).play();
-        }
+
+        au_never_2.cloneNode(true).play();
+
     })
-    //-------------------------------------------------------4
+    //-------------------------------------------------------2.4
     $("#ever_2").click(function () {
-        if (au_ever_2.paused == false) {
-            au_ever_2.pause();
-        } else {
-            au_ever_2.cloneNode(true).play();
-        }
+
+        au_ever_2.cloneNode(true).play();
+
     })
     $("#after_2").click(function () {
-        if (au_after_2.paused == false) {
-            au_after_2.pause();
-        } else {
-            au_after_2.cloneNode(true).play();
-        }
+
+        au_after_2.cloneNode(true).play();
+
     })
     $("#work_is_2").click(function () {
-        if (au_work_is_2.paused == false) {
-            au_work_is_2.pause();
-        } else {
-            au_work_is_2.cloneNode(true).play();
-        }
+
+        au_work_is_2.cloneNode(true).play();
+
     })
     $("#over_2").click(function () {
-        if (au_over_2.paused == false) {
-            au_over_2.pause();
-        } else {
-            au_over_2.cloneNode(true).play();
-        }
+
+        au_over_2.cloneNode(true).play();
+
     })
 
     //-------------------------------------beat
