03b9b4897003007f4c539422924e748ea9bf156f
Zhiwei <Alumnes@inf4.cesnuria>
2018-05-08 17:57:26 +0200

    cloneNode bye
    cloneNode not working with ended listener.

--- js/scripto.js
+++ js/scripto.js
@@ -3,220 +3,297 @@ $(document).ready(function () {
     //-------------------------------------------1
     //-------------------------------------------------------1.1
     $("#work_it_1").click(function () {
-
-        au_work_it_1.cloneNode(true).play();
+        au_work_it_1.addEventListener("ended", function () {
+            $("#work_it_1").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_work_it_1.play();
 
     })
     $("#make_it_1").click(function () {
-
-        au_make_it_1.cloneNode(true).play();
+        au_make_it_1.addEventListener("ended", function () {
+            $("#make_it_1").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_make_it_1.play();
 
     })
     $("#do_it_1").click(function () {
-
-        au_do_it_1.cloneNode(true).play();
+        au_do_it_1.addEventListener("ended", function () {
+            $("#do_it_1").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_do_it_1.play();
 
     })
     $("#makes_us_1").click(function () {
-
-        au_makes_us_1.cloneNode(true).play();
+        au_makes_us_1.addEventListener("ended", function () {
+            $("#makes_us_1").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_makes_us_1.play();
 
     })
 
     //-------------------------------------------------------1.2
     $("#harder_1").click(function () {
-
-        au_harder_1.cloneNode(true).play();
+        au_harder_1.addEventListener("ended", function () {
+            $("#harder_1").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_harder_1.play();
 
     })
     $("#better_1").click(function () {
-
-        au_better_1.cloneNode(true).play();
+        au_better_1.addEventListener("ended", function () {
+            $("#better_1").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_better_1.play();
 
     })
     $("#faster_1").click(function () {
-
-        au_faster_1.cloneNode(true).play();
+        au_faster_1.addEventListener("ended", function () {
+            $("#faster_1").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_faster_1.play();
 
     })
     $("#stronger_1").click(function () {
-
-        au_stronger_1.cloneNode(true).play();
+        au_stronger_1.addEventListener("ended", function () {
+            $("#stronger_1").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_stronger_1.play();
 
     })
     //-------------------------------------------------------1.3
     $("#more_than_1").click(function () {
-
-        au_more_than_1.cloneNode(true).play();
+        au_more_than_1.addEventListener("ended", function () {
+            $("#more_than_1").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_more_than_1.play();
 
     })
     $("#hour_1").click(function () {
-
-        au_hour_1.cloneNode(true).play();
+        au_our_1.addEventListener("ended", function () {
+            $("#hour_1").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_hour_1.play();
 
     })
     $("#our_1").click(function () {
-
-        au_our_1.cloneNode(true).play();
+        au_our_1.addEventListener("ended", function () {
+            $("#our_1").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_our_1.play();
 
     })
     $("#never_1").click(function () {
-
-        au_never_1.cloneNode(true).play();
+        au_never_1.addEventListener("ended", function () {
+            $("#never_1").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_never_1.play();
 
     })
     //-------------------------------------------------------1.4
     $("#ever_1").click(function () {
-
-        au_ever_1.cloneNode(true).play();
+        au_ever_1.addEventListener("ended", function () {
+            $("#ever_1").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_ever_1.play();
 
     })
     $("#after_1").click(function () {
 
-        au_after_1.cloneNode(true).play();
+        au_after_1.addEventListener("ended", function () {
+            $("#after_1").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_after_1.play();
 
     })
     $("#work_is_1").click(function () {
 
-        au_work_is_1.cloneNode(true).play();
+        au_work_is_1.addEventListener("ended", function () {
+            $("#work_is_1").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_work_is_1.play();
 
     })
     $("#over_1").click(function () {
 
-        au_over_1.cloneNode(true).play();
+        au_over_1.addEventListener("ended", function () {
+            $("#over_1").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_over_1.play();
 
     })
 
     //--------------------------------------2
     //-------------------------------------------------------2.1
-    
-    
-    //----------------------------------------cloneNode :( <-------- allé voy
+
+
+    //----------------------------------------cloneNode :( 
     $("#work_it_2").click(function () {
-        
-         au_work_it_2.addEventListener("ended", function () {
+        //au_work_it_2.cloneNode(true).play();        
+        au_work_it_2.addEventListener("ended", function () {
             $("#work_it_2").css("box-shadow", "0px 0px 0px 0px #000");
         })
-         
+
         au_work_it_2.play();
     })
-    
-        
+
+
     $("#make_it_2").click(function () {
 
-        au_make_it_2.cloneNode(true).play();
+        au_make_it_2.addEventListener("ended", function () {
+            $("#make_it_2").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_make_it_2.play();
 
     })
     $("#do_it_2").click(function () {
 
-        au_do_it_2.cloneNode(true).play();
+        au_do_it_2.addEventListener("ended", function () {
+            $("#do_it_2").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_do_it_2.play();
 
     })
     $("#makes_us_2").click(function () {
-
-        au_makes_us_2.cloneNode(true).play();
+        au_makes_us_2.addEventListener("ended", function () {
+            $("#makes_us_2").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_makes_us_2.play();
 
     })
 
     //-------------------------------------------------------2.2
     $("#harder_2").click(function () {
 
-        au_harder_2.cloneNode(true).play();
+        au_harder_2.addEventListener("ended", function () {
+            $("#harder_2").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_harder_2.play();
 
     })
     $("#better_2").click(function () {
-
-        au_better_2.cloneNode(true).play();
+        au_better_2.addEventListener("ended", function () {
+            $("#better_2").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_better_2.play();
 
     })
     $("#faster_2").click(function () {
-
-        au_faster_2.cloneNode(true).play();
+        au_faster_2.addEventListener("ended", function () {
+            $("#faster_2").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_faster_2.play();
 
     })
     $("#stronger_2").click(function () {
-
-        au_stronger_2.cloneNode(true).play();
+        au_stronger_2.addEventListener("ended", function () {
+            $("#stronger_2").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_stronger_2.play();
 
     })
     //-------------------------------------------------------2.3
     $("#more_than_2").click(function () {
-
-        au_more_than_2.cloneNode(true).play();
+        au_more_than_2.addEventListener("ended", function () {
+            $("#more_than_2").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_more_than_2.play();
 
     })
     $("#hour_2").click(function () {
-
-        au_hour_2.cloneNode(true).play();
+        au_hour_2.addEventListener("ended", function () {
+            $("#hour_2").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_hour_2.play();
 
     })
     $("#our_2").click(function () {
-
-        au_our_2.cloneNode(true).play();
+        au_our_2.addEventListener("ended", function () {
+            $("#our_2").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_our_2.play();
 
     })
     $("#never_2").click(function () {
-
-        au_never_2.cloneNode(true).play();
+        au_never_2.addEventListener("ended", function () {
+            $("#never_2").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_never_2.play();
 
     })
     //-------------------------------------------------------2.4
     $("#ever_2").click(function () {
-
-        au_ever_2.cloneNode(true).play();
+        au_ever_2.addEventListener("ended", function () {
+            $("#ever_2").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_ever_2.play();
 
     })
     $("#after_2").click(function () {
-
-        au_after_2.cloneNode(true).play();
+        au_after_2.addEventListener("ended", function () {
+            $("#after_2").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_after_2.play();
 
     })
     $("#work_is_2").click(function () {
-
-        au_work_is_2.cloneNode(true).play();
+        au_work_is_2.addEventListener("ended", function () {
+            $("#work_is_2").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_work_is_2.play();
 
     })
     $("#over_2").click(function () {
-
-        au_over_2.cloneNode(true).play();
+        au_over_2.addEventListener("ended", function () {
+            $("#over_2").css("box-shadow", "0px 0px 0px 0px #000");
+        })
+        au_over_2.play();
 
     })
 
     //-------------------------------------beat
+    var b = 0;
     $(".beat").click(function () {
-        if (au_beat.paused == false) {
+        if (au_beat.paused == true) {
+            au_beat.play();
+            $(this).css("background-color", "#17C5E8");
+            //$(".button").css("box-shadow", "0px 0px 30px 0px #4CE1FF");
+            b = 1;
+        
+        } else {
             au_beat.pause();
             au_beat.currentTime = 0;
             $(this).css("background-color", "#66D3E8");
-            $(".button").css("box-shadow", "0px 0px 0px 0px #000");
-        } else {
-            au_beat.play();
-            $(this).css("background-color", "#17C5E8");
-            $(".button").css("box-shadow", "0px 0px 30px 0px #4CE1FF");
-            $(".button.high, .button.beat").css("box-shadow", "0px 0px 0px 0px #000");
+            //$(".button").css("box-shadow", "0px 0px 0px 0px #000");
+            b = 0;
         }
     })
 
 
     //-------------------------------------high
-    var f = 0;
+    var h = 0;
     $(".high").click(function () {
-        if (f == 0) {
+        if (h == 0) {
             $(this).css("background-color", "#FFBE1A");
             $("#high").css("display", "block");
             $("#normal").css("display", "none");
-            f = 1;
+            h = 1;
+          
         } else {
             $(this).css("background-color", "#FFCC4C");
             $("#high").css("display", "none");
             $("#normal").css("display", "block");
-            f = 0;
+            h = 0;
         }
     })
 
     $(".button").click(function () {
-        if (f == 1) {
-            $(this).css("box-shadow", "0px 0px 30px 0px #FFCC4C");
+    
+        if (b == 1) {
+            $(this).css("box-shadow", "0px 0px 40px 0px #4CE1FF");
+            $(".high, .beat").css("box-shadow", "0px 0px 0px 0px #000");
+        }
+        if (h == 1) {
+            $(this).css("box-shadow", "0px 0px 40px 0px #FFCC4C");
             $(".high, .beat").css("box-shadow", "0px 0px 0px 0px #000");
         }
     })
